d76964af618e6c006bd41583ff01ee5822170182
jiantengtoo <aiyo321@gmail.com>
2021-08-09 14:58:50 +0800

    change center

--- client/src/map/CustomMap.tsx
+++ client/src/map/CustomMap.tsx
@@ -15,7 +15,7 @@ function CustomMap() {
 
   return (
     <>
-      <MapContainer center={ [0,0] } zoom={13} style={{height:'90vh'}}>
+      <MapContainer center={ [1.285194, 103.8522982] } zoom={14} style={{height:'90vh'}}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
